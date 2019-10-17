31c5b5f86d5d952c0174ad77dd039963214c29f3
elwiiman <emmanuel.sanro@gmail.com>
2019-10-17 03:26:42 -0500

    New tatoos cards created in profile, still missing images

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { isAuth } = require("../helpers/middlewaresAuth");
+const { formatDate } = require("../helpers/formatDataTime");
+const Tatoo = require("../models/Tatoo");
 
 /* GET home page */
 router.get("/", isAuth, (req, res, next) => {
@@ -15,7 +17,17 @@ router.get("/home", isAuth, (req, res, next) => {
 
 router.get("/profile", isAuth, (req, res, next) => {
   const { user } = req;
-  res.render("profile", { title: "Profile", user });
+  let help = {};
+  help.formatDate = formatDate;
+  Tatoo.find({ authorArtist: user._id }).then(tatoos => {
+    console.log(tatoos);
+    res.render("profile", { title: "Profile", user, tatoos, help });
+  });
+
+  router.get("/card", isAuth, (req, res, next) => {
+    const { user } = req;
+    res.render("pruebaCard", { user });
+  });
 });
 
 module.exports = router;
