08af072ae637d93911e0bf2b4909f03c70407573
elwiiman <emmanuel.sanro@gmail.com>
2019-10-19 02:32:15 -0500

    Tatoo CRUDE Completed

--- routes/tatoo.js
+++ routes/tatoo.js
@@ -51,7 +51,6 @@ router.post("/new", isAuth, uploader.single("image"), (req, res) => {
       .catch(error => {
         res.render("newTatoo", {
           title: "New Tatoo",
-          create: true,
           user,
           error
         });
@@ -60,17 +59,79 @@ router.post("/new", isAuth, uploader.single("image"), (req, res) => {
 });
 
 //EDIT ROUTES
-router.get("/edit/:id", (req, res) => {
+router.get("/edit/:id", isAuth, (req, res) => {
+  const { user } = req;
   const { id } = req.params;
   Tatoo.findById(id)
     .then(tatoo => {
-      res.render("editTatoo", { title: "Edit Tatoo", tatoo, options });
+      res.render("editTatoo", { title: "Edit Tatoo", tatoo, options, user });
     })
     .catch(err => console.log(err));
 });
 
+router.post("/edit/:id", isAuth, uploader.single("image"), (req, res) => {
+  const { id } = req.params;
+  const { user } = req;
+  const {
+    user: { _id: authorArtist }
+  } = req;
+
+  const { name, price: value, currency, exec_time, description } = req.body;
+  let image;
+
+  if (!name || !value || !description) {
+    let errorMessage = "Required fields should be filled";
+    Tatoo.findById(id).then(tatoo => {
+      return res.render("editTatoo", {
+        title: "Edit Tatoo",
+        tatoo,
+        user,
+        errorMessage
+      });
+    });
+  } else {
+    if (!req.file) {
+      Tatoo.findById(id, "image")
+        .then(tatooImage => {
+          image = tatooImage.image;
+          let tatoo = {
+            authorArtist,
+            price: { value, currency },
+            name,
+            description,
+            image,
+            exec_time
+          };
+
+          Tatoo.findByIdAndUpdate(id, { $set: tatoo }, { new: true }).then(
+            tatoo => {
+              res.redirect("/profile");
+            }
+          );
+        })
+        .catch(err => console.log(err));
+    } else {
+      image = req.file.secure_url;
+      let tatoo = {
+        authorArtist,
+        price: { value, currency },
+        name,
+        description,
+        image,
+        exec_time
+      };
+
+      Tatoo.findByIdAndUpdate(id, { $set: tatoo }, { new: true }).then(
+        tatoo => {
+          res.redirect("/profile");
+        }
+      );
+    }
+  }
+});
+
 /// DELETE TATOO ROUTE
-router.get("/delete/:id", (req, res) => {
+router.get("/delete/:id", isAuth, (req, res) => {
   const { id } = req.params;
   console.log("this is id", id);
   Tatoo.findByIdAndDelete(id)
